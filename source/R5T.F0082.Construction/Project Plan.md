a5c04b89f5d4b2bbbe6a1d68dfd13defc1c44556
davidcoats <david@getsafetycone.com>
2022-11-18 18:06:42 -0800

    Initial commit.

---
+++ source/R5T.F0082.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0082.Construction
+Construction console project for the R5T.F0082 library.
---
+++ source/R5T.F0082/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0082
+{
+	/// <summary>
+	/// Code file system survey functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
